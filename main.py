7090eaeed9fc37bd26f786c270f7406261ee7a32
seoyoon1209 <kim0502020@naver.com>
2026-07-22 23:34:18 -0700

    CORS에 배포된 프론트 origin 추가 (smsf-0pzo.onrender.com)

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ app = FastAPI(title="Hotel Reservation Cancellation Prediction API", lifespan=li
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://smsf-0pzo.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
